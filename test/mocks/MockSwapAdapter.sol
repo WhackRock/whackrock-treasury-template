f0fe4841c8fc5a6454942d0e26884e0eab199e50
WhackRock <whaackroock@gmail.com>
2025-05-15 09:46:23 +0200

    test: rebalance(...) coverage

--- test/mocks/MockSwapAdapter.sol
+++ test/mocks/MockSwapAdapter.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.20;
 import "../../src/core/interfaces/ISwapAdapter.sol";
 
 contract MockSwapAdapter is ISwapAdapter {
+    bytes public lastExecuteData;
+    
     function execute(bytes calldata data) 
         external 
         payable 
         override 
         returns (bool success) 
     {
+        lastExecuteData = data;
         return true; // Always succeed for testing
     }
 } 
