774b91b4665cff2ffbe063486516cd3a88538e87
WhackRock <whaackroock@gmail.com>
2025-05-22 22:01:48 +0200

    feat(src, test): Added WHACKROCK ascii

--- src/WhackRockFundRegistry.sol
+++ src/WhackRockFundRegistry.sol
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: BUSL-1.1
-// © 2024 WhackRock Labs – All rights reserved.
-// WHACKROCK – FUND REGISTRY AND FACTORY
+
+/*
+ *  
+ *   oooooo   oooooo     oooo ooooo   ooooo       .o.         .oooooo.   oooo    oooo ooooooooo.     .oooooo.     .oooooo.   oooo    oooo 
+ *   `888.    `888.     .8'  `888'   `888'      .888.       d8P'  `Y8b  `888   .8P'  `888   `Y88.  d8P'  `Y8b   d8P'  `Y8b  `888   .8P'  
+ *    `888.   .8888.   .8'    888     888      .8"888.     888           888  d8'     888   .d88' 888      888 888           888  d8'    
+ *     `888  .8'`888. .8'     888ooooo888     .8' `888.    888           88888[       888ooo88P'  888      888 888           88888[      
+ *      `888.8'  `888.8'      888     888    .88ooo8888.   888           888`88b.     888`88b.    888      888 888           888`88b.    
+ *       `888'    `888'       888     888   .8'     `888.  `88b    ooo   888  `88b.   888  `88b.  `88b    d88' `88b    ooo   888  `88b.  
+ *        `8'      `8'       o888o   o888o o88o     o8888o  `Y8bood8P'  o888o  o888o o888o  o888o  `Y8bood8P'   `Y8bood8P'  o888o  o888o 
+ *  
+ *    WHACKROCK – FUND REGISTRY AND FACTORY
+ *    © 2024 WhackRock Labs – All rights reserved.
+ */
+
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import {IAerodromeRouter} from "./interfaces/IRouter.sol";
 import {WhackRockFund} from "./WhackRockFundV5_ERC4626_Aerodrome_SubGEvents.sol";
 import {IWhackRockFundRegistry} from "./interfaces/IWhackRockFundRegistry.sol";
+
 
 contract WhackRockFundRegistry is IWhackRockFundRegistry, Ownable {
     IAerodromeRouter public immutable aerodromeRouter;
--- src/WhackRockFundV5_ERC4626_Aerodrome_SubGEvents.sol
+++ src/WhackRockFundV5_ERC4626_Aerodrome_SubGEvents.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: BUSL-1.1
-// © 2024 WhackRock Labs – All rights reserved.
-// WHACKROCK – AGENT‑MANAGED WEIGHTED VAULT
+
+/*
+ *  
+ *   oooooo   oooooo     oooo ooooo   ooooo       .o.         .oooooo.   oooo    oooo ooooooooo.     .oooooo.     .oooooo.   oooo    oooo 
+ *   `888.    `888.     .8'  `888'   `888'      .888.       d8P'  `Y8b  `888   .8P'  `888   `Y88.  d8P'  `Y8b   d8P'  `Y8b  `888   .8P'  
+ *    `888.   .8888.   .8'    888     888      .8"888.     888           888  d8'     888   .d88' 888      888 888           888  d8'    
+ *     `888  .8'`888. .8'     888ooooo888     .8' `888.    888           88888[       888ooo88P'  888      888 888           88888[      
+ *      `888.8'  `888.8'      888     888    .88ooo8888.   888           888`88b.     888`88b.    888      888 888           888`88b.    
+ *       `888'    `888'       888     888   .8'     `888.  `88b    ooo   888  `88b.   888  `88b.  `88b    d88' `88b    ooo   888  `88b.  
+ *        `8'      `8'       o888o   o888o o88o     o8888o  `Y8bood8P'  o888o  o888o o888o  o888o  `Y8bood8P'   `Y8bood8P'  o888o  o888o 
+ *  
+ *    AGENT‑MANAGED WEIGHTED FUND   
+ *    © 2024 WhackRock Labs – All rights reserved.
+ */
+
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
